95b31fda834148a039fc479470171ef58aee288c
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2019-04-02 15:12:27 +0200

    Fix highres output of overlay masks

--- pagesegmentation/lib/predictor.py
+++ pagesegmentation/lib/predictor.py
@@ -81,13 +81,14 @@ class Predictor:
             color_mask = label_to_colors(pred)
             foreground = np.stack([(1 - data.image / 255)] * 3, axis=-1)
             inv_binary = np.stack([data.binary] * 3, axis=-1)
-            overlay_mask = np.ndarray.astype(color_mask * foreground, dtype=np.uint8)
-            inverted_overlay_mask = np.ndarray.astype(color_mask * inv_binary, dtype=np.uint8)
 
             if self.settings.high_res_output:
                 color_mask = misc.imresize(color_mask[data.xpad:, data.ypad:], data.original_shape, interp="nearest")
                 foreground = misc.imresize(foreground[data.xpad:, data.ypad:], data.original_shape)
                 inv_binary = misc.imresize(inv_binary[data.xpad:, data.ypad:], data.original_shape, interp="nearest")
+
+            overlay_mask = np.ndarray.astype(color_mask * foreground, dtype=np.uint8)
+            inverted_overlay_mask = np.ndarray.astype(color_mask * inv_binary, dtype=np.uint8)
 
             img_io.imsave(os.path.join(self.settings.output, "color", filename), color_mask)
             img_io.imsave(os.path.join(self.settings.output, "overlay", filename), overlay_mask)
