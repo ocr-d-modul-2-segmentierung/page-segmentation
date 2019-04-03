61eaddbc115a99b1695084bd5696376d95b5192d
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2019-04-03 15:33:15 +0200

    Add postprocessing to predict-json too

--- pagesegmentation/scripts/predict_json.py
+++ pagesegmentation/scripts/predict_json.py
@@ -4,6 +4,7 @@ import os
 import tqdm
 
 from pagesegmentation.lib.dataset import DatasetLoader
+from pagesegmentation.lib.postprocess import vote_connected_component_class
 from pagesegmentation.lib.predictor import Predictor, PredictSettings
 
 def main():
@@ -17,6 +18,8 @@ def main():
     parser.add_argument("--eval", type=str, nargs="*", default=[], help="JSON file with data to use for evaluation")
     parser.add_argument("--keep_low_res", action="store_true",
                         help="keep low resolution prediction instead of rescaling output to orignal image size")
+    parser.add_argument("--cc_majority", action="store_true",
+                        help="classify all pixels of each connected component as most frequent class")
     args = parser.parse_args()
 
     os.makedirs(args.output, exist_ok=True)
@@ -24,13 +27,18 @@ def main():
     dataset_loader = DatasetLoader(args.target_line_height, prediction=True)
     data = dataset_loader.load_data_from_json(args.eval, "eval")
 
+    post_processors = []
+    if args.cc_majority:
+        post_processors += [vote_connected_component_class]
+
     print("Creating net")
     settings = PredictSettings(
         mode='meta',
         network=os.path.abspath(args.load),
         output=args.output,
         high_res_output=not args.keep_low_res,
-        n_classes=4
+        n_classes=4,
+        post_process=post_processors
     )
     predictor = Predictor(settings)
 
