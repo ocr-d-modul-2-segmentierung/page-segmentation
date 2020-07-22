cc0bd22bf2105ce8b48992664f1b9d8eced5d0be
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2020-07-22 13:39:46 +0200

    Bump version: 0.3.0 → 0.4.0
    This version removes all CLI parts and is therefore not completely
    compatible with 0.3.0. Parts that were in the `scripts` module, but also
    useful for non-cli usage were moved into a suitable `lib` submodule.
    Names of functions haven't changed during the move.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ocr4all_pixel_classifier',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(),
     long_description=long_description,
 
