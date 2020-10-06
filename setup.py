34b8bbf24edaaae8ab0b7b3f31c2089ed3ad3f0c
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2020-10-06 16:33:37 +0200

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ocr4all_pixel_classifier',
-    version='0.6.1',
+    version='0.6.2',
     packages=find_packages(),
     long_description=long_description,
 
